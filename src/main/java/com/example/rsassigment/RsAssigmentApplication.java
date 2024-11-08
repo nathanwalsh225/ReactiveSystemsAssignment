1e141fec2eb1639813514accd707ec838dcbb47b
nathanwalsh225 <nathanwalsh225@gmail.com>
2024-11-08 13:50:15 +0000

    Initial Commit

---
+++ src/main/java/com/example/rsassigment/RsAssigmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.rsassigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsAssigmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsAssigmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rsassigment/RsAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rsassigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsAssigmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
