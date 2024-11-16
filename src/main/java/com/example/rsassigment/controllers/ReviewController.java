c731a59032b5e46dfa9334d4c71f97448ffee0c5
nathanwalsh225 <nathanwalsh225@gmail.com>
2024-11-16 20:00:21 +0000

    ReviewController push with both methods for clarity

--- src/main/java/com/example/rsassigment/controllers/ReviewController.java
+++ src/main/java/com/example/rsassigment/controllers/ReviewController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import com.example.rsassigment.service.ReviewService;
@@ -19,40 +20,43 @@ public class ReviewController {
     }
 
     @GetMapping(path ="/reviewStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Review> reviewStream(@ModelAttribute("criteria") Criteria criteria, Model model) {
+    public Flux<Review> reviewStream(@RequestParam(required = false) String restaurantName, @RequestParam(required = false) Integer rating) {
         //url params
-//        model.addAttribute("criteria", criteria);
-//        System.out.println("Name: " + criteria.getRestaurantName());
-//        System.out.println("Rating: " + criteria.getRating());
 
+
+//      Flux<Review> reviewFlux = reviewService.streamReviewUpdates()
+//                .filter(review -> {
+//                    System.out.println("Review Name: " + review.getRestaurantName());
+//                    System.out.println("Criteria Name: " + restaurantName);
+//                    System.out.println("Review Rating: " + review.getRating());
+//                    System.out.println("Criteria Rating: " + rating);
 //
-        Flux<Review> reviewFlux = reviewService.streamReviewUpdates();
+//                    //Validation for if the critera matches the review, if it does return true and continue
+//                    //onto doOnNext, if not return false and just return the reviewflux to the reviews.html
+//                    return reviewService.validateCriteria(restaurantName, rating, review);
+//                })
+//                .doOnNext(review -> {
+//                    reviewService.insertReview(review).subscribe(
+//                            success -> System.out.println("Saved to DB: " + review),
+//                            error -> System.err.println("Failed to save to DB: " + error.getMessage())
+//                    );
+//                })
+//                .doOnError(error -> {
+//                    System.out.println("Error: " + error.getMessage());
+//                });
 
-        reviewFlux.subscribe(review -> {
-            System.out.println("Review: " + review.getRestaurantName());
-            System.out.println("Rating: " + review.getRating());
-
-           Boolean reviewAdded = reviewService.insertReview(review.getRestaurantName(), review.getAddress(), review.getRating(), review.getMenuItem(), review.getOpeningHours(), review.getClosingHours(), review.getDeliveryAvailable(), review.getReviewText());
-
-            if(reviewAdded){
-                System.out.println("Review added successfully");
-            }else{
-                System.out.println("Review not added");
-            }
-        });
-
-        return reviewFlux;
-
-
-
-//        return reviewService.streamReviewUpdates().doOnNext((review -> {
-//            System.out.println("Review: " + review.getRestaurantName());
-//
-//
-//
-//
-//
-//        }));
+        return reviewService.streamReviewUpdates()
+                .filter(review -> {
+                    // Each review is passed to the service layer where it is validated against the criteria
+                    // to see if it should be inserting into the db, if the criteria is met, the review is added
+                    // the return true, returns the review to the front end to be displayed displayed regardless
+                    // if the review was saved to the DB
+                    reviewService.insertReview(restaurantName, rating, review).subscribe(
+                            success -> System.out.println("Saved to DB: " + review),
+                            error -> System.err.println("Failed to save to DB: " + error.getMessage())
+                    );
+                    return true;
+                });
     }
 
 
