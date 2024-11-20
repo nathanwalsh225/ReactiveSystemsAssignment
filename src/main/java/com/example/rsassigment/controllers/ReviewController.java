33c9108fa6a175d3aa5f050196696a79d7168178
K00269862 <nathanwalsh225@gmail.com>
2024-11-20 11:45:44 +0000

    Changes to build to deal with mongoDB issues

--- src/main/java/com/example/rsassigment/controllers/ReviewController.java
+++ src/main/java/com/example/rsassigment/controllers/ReviewController.java
@@ -21,21 +21,18 @@ public class ReviewController {
     @GetMapping(path ="/reviewStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Review> reviewStream(@RequestParam(required = false) String restaurantName, @RequestParam(required = false) Integer rating) {
 
-//        return reviewService.streamReviewUpdates()
-//                .filter(review -> {
-//                    // Each review is passed to the service layer where it is validated against the criteria
-//                    // to see if it should be inserting into the db, if the criteria is met, the review is added
-//                    // the return true, returns the review to the front end to be displayed displayed regardless
-//                    // if the review was saved to the DB
-//                    reviewService.insertReview(restaurantName, rating, review).subscribe(
-//                            success -> System.out.println("Saved to DB: " + review),
-//                            error -> System.err.println("Failed to save to DB: " + error.getMessage())
-//                    );
-//                    return true;
-//                });
-
-
-        return null;
+        return reviewService.streamReviewUpdates()
+                .filter(review -> {
+                    // Each review is passed to the service layer where it is validated against the criteria
+                    // to see if it should be inserting into the db, if the criteria is met, the review is added
+                    // the return true, returns the review to the front end to be displayed displayed regardless
+                    // if the review was saved to the DB
+                    reviewService.insertReview(restaurantName, rating, review).subscribe(
+                            success -> System.out.println("Saved to DB: " + review),
+                            error -> System.err.println("Failed to save to DB: " + error.getMessage())
+                    );
+                    return true;
+                });
     }
 
     @GetMapping("/reviews/saved")
